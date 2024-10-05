f756136848c29f938a8bf85eea75f7ff3cb9822f
Dzhansu Zalim <djansuevil@gmail.com>
2024-10-05 12:19:49 +0300

    initial setup

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from db import db
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+app.config.from_object('config.DevelopmentConfig')
+db.init_app(app)
+api = Api(app)
+migrate = Migrate(app, db)
+CORS(app)
