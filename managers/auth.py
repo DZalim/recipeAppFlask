bb3a10727978bef1594878081a5be95cc4553aa7
Dzhansu Zalim <djansuevil@gmail.com>
2024-10-05 17:03:54 +0300

    add verify token

--- managers/auth.py
+++ managers/auth.py
@@ -2,6 +2,11 @@ from datetime import datetime, timedelta
 
 import jwt
 from decouple import config
+from flask_httpauth import HTTPTokenAuth
+from werkzeug.exceptions import Unauthorized
+
+from db import db
+from models import UserModel
 
 
 class AuthManager:
@@ -19,3 +24,29 @@ class AuthManager:
             )
         except Exception as e:
             raise e
+
+
+    @staticmethod
+    def decode_token(token):
+        try:
+            info = jwt.decode(
+                jwt=token,
+                key=config('SECRET_KEY'),
+                algorithms=["HS256"]
+            )
+            return info['sub']  # user_id
+
+        except Exception as ex:
+            raise ex
+
+
+    auth = HTTPTokenAuth(scheme='Bearer')
+
+    @staticmethod
+    @auth.verify_token
+    def verify_token(token):
+        try:
+            user_id = AuthManager.decode_token(token)
+            return db.session.execute(db.select(UserModel).filter_by(id=user_id)).scalar()
+        except Exception as ex:
+            raise Unauthorized("Invalid or missing token")
